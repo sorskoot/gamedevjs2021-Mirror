15379ebc443487d56e03682cbceacb887cf8e13e
Timmy Kokke <mental_c@hotmail.com>
2021-04-19 21:52:57 +0200

    playing music

--- src/components/game.component.js
+++ src/components/game.component.js
@@ -1,6 +1,14 @@
 AFRAME.registerComponent('game', {
    schema: {},
-   init: function () {  },
+   init: function () { 
+      this.musicGame = document.getElementById("music-game");
+      this.el.sceneEl.addEventListener('enter-vr', () => {
+         this.musicGame.play();
+     })
+     this.el.sceneEl.addEventListener('exit-vr', () => {
+         this.musicGame.pause();
+     })
+   },
    update: function (oldData) { },
    tick: function (time, timeDelta) { },
    tock: function (time, timeDelta, camera){ },
